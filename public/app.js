82b462d762deb627ffea4aad7ee9c44c544be2ab
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-06 19:20:51 -0500

    Initial dynamic HTML generate

---
+++ public/app.js
@@ -0,0 +1,25 @@
+$( () => {
+
+  //Page Load Inovation
+  app.loadContent();
+
+}); //
+
+//Element Collection/Generating
+const $container = $('<section/>').attr('id', 'container');
+const $h1Title = $('<h1/>').text('To Do List');
+const $input = $('<input/>').attr('type', 'text');
+const $ul = $('<ul/>');
+const $initialList = ['First', 'Second', 'Third'];
+
+const app = {
+  loadContent() {
+    $('body').append($container);
+    $container.append($h1Title, $input, $ul);
+    $ul.each(function(i){
+      for (let i = 0; i < $initialList.length; i++) {
+        $(this).append('<li><span>X</span> ' + $initialList[i] + '</li>');
+      }
+    });
+  }
+};
