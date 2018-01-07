75bf49c37cc95d9a9c62008df7f8c1152d989bf0
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-06 21:03:43 -0500

    Remove template LI

--- public/app.js
+++ public/app.js
@@ -10,23 +10,28 @@ const $container = $('<section/>').attr('id', 'container');
 const $h1Title = $('<h1>To Do List<i class="fa fa-plus-square" aria-hidden="true"></i></h1>');
 const $input = $('<input/>').attr('type', 'text').attr('placeholder', 'Add New To Do');
 const $ul = $('<ul/>');
-const $initialList = ['First', 'Second', 'Third'];
 
+//APP Object
 const app = {
   loadContent() {
     $('body').append($container);
     $container.append($h1Title, $input, $ul);
-    $ul.each(function(i){
-      for (let i = 0; i < $initialList.length; i++) {
-        $(this).append('<li><span><i class="fa fa-minus-square" aria-hidden="true"></i></span> ' + $initialList[i] + '</li>');
-      }
-    });
+    // this.templateList(); //Disabled, used for initial build of template <li>
     this.intitFunctionality();
   },
   intitFunctionality() {
     this.strikethrough();
     this.deleteItem();
     this.addItem();
+  },
+  templateList() {
+    //Disabled, used for initial build of template <li>
+    const $initialList = ['First', 'Second', 'Third'];
+    $ul.each(function(i){
+      for (let i = 0; i < $initialList.length; i++) {
+        $(this).append('<li><span><i class="fa fa-minus-square" aria-hidden="true"></i></span> ' + $initialList[i] + '</li>');
+      }
+    });
   },
   strikethrough() {
     $('ul').on('click', 'li', (e) => {
