93bd6882168c76992c6e9068ff097e1f57507f20
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-06 20:57:26 -0500

    Font Awesome implentation

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ $( () => {
 
 //Element Collection/Generating
 const $container = $('<section/>').attr('id', 'container');
-const $h1Title = $('<h1/>').text('To Do List');
+const $h1Title = $('<h1>To Do List<i class="fa fa-plus-square" aria-hidden="true"></i></h1>');
 const $input = $('<input/>').attr('type', 'text').attr('placeholder', 'Add New To Do');
 const $ul = $('<ul/>');
 const $initialList = ['First', 'Second', 'Third'];
@@ -18,7 +18,7 @@ const app = {
     $container.append($h1Title, $input, $ul);
     $ul.each(function(i){
       for (let i = 0; i < $initialList.length; i++) {
-        $(this).append('<li><span>X</span> ' + $initialList[i] + '</li>');
+        $(this).append('<li><span><i class="fa fa-minus-square" aria-hidden="true"></i></span> ' + $initialList[i] + '</li>');
       }
     });
     this.intitFunctionality();
@@ -45,7 +45,7 @@ const app = {
     $input.on('keypress', (e) => {
       if (e.which === 13) {
         let $valueText = $(e.currentTarget).val();
-        $ul.append('<li><span>X</span> ' + $valueText + '</li>');
+        $ul.append('<li><span><i class="fa fa-minus-square" aria-hidden="true"></i></span> ' + $valueText + '</li>');
         $(e.currentTarget).val('');
       }
     });
